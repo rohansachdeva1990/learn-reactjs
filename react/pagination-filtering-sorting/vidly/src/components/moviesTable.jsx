a004f97492f4fa2e975f7215b7ecbab636cff5b1
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-09-10 23:46:06 +0200

    [React] Extracted table

--- react/pagination-filtering-sorting/vidly/src/components/moviesTable.jsx
+++ react/pagination-filtering-sorting/vidly/src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Like from './common/like';
-import TableHeader from './common/tableHeader';
-import TableBody from './common/tableBody';
+import Table from './common/table';
 
 class MoviesTable extends Component {
   columns = [
@@ -29,16 +28,13 @@ class MoviesTable extends Component {
 
   render() {
     const { movies, onSort, sortColumn } = this.props;
-
     return (
-      <table className='table'>
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody columns={this.columns} data={movies} />
-      </table>
+      <Table
+        columns={this.columns}
+        sortColumn={sortColumn}
+        onSort={onSort}
+        data={movies}
+      />
     );
   }
 }
