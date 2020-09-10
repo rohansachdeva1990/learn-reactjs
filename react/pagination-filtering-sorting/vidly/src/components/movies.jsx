69966181e7ca697467871e8ea23744723086a891
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-09-10 23:54:55 +0200

    [React] Sorting, filtering and pagination finitoo! + Cleanup

--- react/pagination-filtering-sorting/vidly/src/components/movies.jsx
+++ react/pagination-filtering-sorting/vidly/src/components/movies.jsx
@@ -51,8 +51,7 @@ class Movies extends Component {
     this.setState({ sortColumn });
   };
 
-  render() {
-    const { length: count } = this.state.movies;
+  getPagedData = () => {
     const {
       pageSize,
       currentPage,
@@ -61,15 +60,23 @@ class Movies extends Component {
       movies: allMovies,
     } = this.state;
 
-    if (count === 0) return <p>There are no movies in the database.</p>;
-
-    // If selected genre is truthy
     const filtered =
       selectedGenre && selectedGenre._id
         ? allMovies.filter(m => m.genre._id === selectedGenre._id)
         : allMovies;
     const sorted = _.orderBy(filtered, [sortColumn.path], sortColumn.order);
     const movies = paginate(sorted, currentPage, pageSize);
+
+    return { totalCount: filtered.length, data: movies };
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    const { pageSize, currentPage, sortColumn } = this.state;
+
+    if (count === 0) return <p>There are no movies in the database.</p>;
+
+    const { totalCount, data: movies } = this.getPagedData();
 
     return (
       <div className='row'>
@@ -81,7 +88,7 @@ class Movies extends Component {
           />
         </div>
         <div className='col'>
-          <p>Showing {filtered.length} movies in the database.</p>
+          <p>Showing {totalCount} movies in the database.</p>
           <MoviesTable
             movies={movies}
             onLike={this.handleLike}
@@ -90,7 +97,7 @@ class Movies extends Component {
             sortColumn={sortColumn}
           />
           <Pagination
-            itemsCount={filtered.length}
+            itemsCount={totalCount}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
